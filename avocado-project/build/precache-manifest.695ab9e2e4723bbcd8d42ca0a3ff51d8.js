fa325b7d251dd0e3b4f88f7046844ab3a1760028
williamfiallos <wfiallos10@gmail.com>
2019-04-13 14:38:00 -0400

    deploy test

---
+++ avocado-project/build/precache-manifest.695ab9e2e4723bbcd8d42ca0a3ff51d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8eada7dac06c4463c6dd",
+    "url": "/static/js/main.867c8cc8.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8eada7dac06c4463c6dd",
+    "url": "/static/css/main.4e99cfc8.chunk.css"
+  },
+  {
+    "revision": "4523ee8057b31c084229de405de63035",
+    "url": "/index.html"
+  }
+];
